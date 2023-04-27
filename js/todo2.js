f7c517c8acb44c6a307fcaf6ccb8ce516868f247
박선영 <sunny.playgnd@gmail.com>
2023-04-27 13:36:37 +0900

    5주차 수업준비

--- js/todo2.js
+++ js/todo2.js
@@ -4,20 +4,16 @@ $(document).ready(function () {
     renderRandomImage();
 });
 
-//현재 시간
 function renderCurrentTime() {
     let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
             let datetime = data["datetime"].substr(11, 5);
-            // let datetime = data['datetime']
-            // console.log(datetime)
             $("#time").text(datetime);
         });
 }
 
-// 명언
 function renderQuote() {
     let url = `https://api.quotable.io/random`;
     fetch(url)
@@ -30,15 +26,13 @@ function renderQuote() {
         });
 }
 
-// 랜덤 사진 꾸미기
 function renderRandomImage() {
     let imageList = [];
-    // 이미지 개수를 변경하려면 i=5의 값을 이미지 개수만큼 바꿔주세요!
     for (i = 0; i < 5; i++) {
         imageList.push(i);
     }
     let imageListLength = imageList.length;
-    let randomImage = Math.floor(Math.random() * (imageListLength)) + 1
-    randomImage = `img/todo_${randomImage}.jpg`
+    let randomImage = Math.floor(Math.random() * (imageListLength)) + 1;
+    randomImage = `img/todo_${randomImage}.jpg`;
     $(document.body).css("background-image", `url(${randomImage})`);
 }
